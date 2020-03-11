87c0d0dda106270e04b3cdc1c8c27898922dbd50
Xiaolong <xiaolong_huang@foxmail.com>
2020-03-11 16:37:41 +1300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="docdoc", # Replace with your own username
+    version="0.0.16",
+    author="Enno",
+    author_email="hi.xiaolonghuang@gmail.com",
+    description="A tool to handle documents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/click-me/docdoc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'sentence_splitter==1.4',
+        'nltk==3.4.5'
+    ],
+    include_package_data = True
+)
+
+
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
